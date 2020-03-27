c0dbbfcf96b9ad21e3d091faa82d48f36e154fb1
sarvathulbasha <basha@uitouxsolutions.com>
2020-03-27 16:09:31 +0530

    init

---
+++ src/main/java/com/example/base/activities/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.base.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.base.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+    }
+}
